199f0fd868b8e88681369bf4b56b4b445656cd74
Hayden Ngo <hoango1607@gmail.com>
2024-05-20 22:03:27 +0930

    chore: clean global css & update SEO

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,4 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, pre-configured with Husky',
+  title: 'World Time Buddy',
+  description: 'World Time Buddy (WTB) clone',
 };
